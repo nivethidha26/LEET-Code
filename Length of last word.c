cf4360cd367351acdb5dbf6175e704a983ab0a18
Nivethidha Krishi A <nivethidha26@gmail.com>
2026-08-18 15:46:11 +0530

    Implement function to find length of last word

---
+++ Length of last word.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+int lengthOfLastWord(char* s) {
+    int len = strlen(s);
+    int i = len - 1;
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+    int count = 0;
+    while (i >= 0 && s[i] != ' ') {
+        count++;
+        i--;
+    }
+    return count;
+}
