6c7ec0fd5e558d45cc980bae5ab30c77a1113f3d
Nivethidha Krishi A <nivethidha26@gmail.com>
2026-09-08 16:39:34 +0530

    Create Check If N and Its Double Exists.c

---
+++ Check If N and Its Double Exists.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+bool checkIfExist(int* arr, int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        for (int j = 0; j < arrSize; j++) {
+            if (i != j && arr[i] == 2 * arr[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
