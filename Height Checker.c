5451b1987ba8c76cfef861d407f2242d600c7241
Nivethidha Krishi A <nivethidha26@gmail.com>
2026-09-08 16:31:52 +0530

    Create Height Checker.c

---
+++ Height Checker.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+int heightChecker(int* heights, int heightsSize) {
+    int* expected = (int*)malloc(heightsSize * sizeof(int));
+    for (int i = 0; i < heightsSize; i++) {
+        expected[i] = heights[i];
+    }
+    qsort(expected, heightsSize, sizeof(int), compare);
+    int count = 0;
+    for (int i = 0; i < heightsSize; i++) {
+        if (heights[i] != expected[i]) {
+            count++;
+        }
+    }
+    free(expected);
+    return count;
+}
